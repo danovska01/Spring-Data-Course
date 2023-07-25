382a829d8d0c8355d9cef0844c8199ca6b8385e7
kristina <kristina.danovska@gmail.com>
2023-07-25 13:37:24 +0300

    add all

---
+++ _12_Spring_Data_Intro_Ex/src/test/java/com/example/_12_spring_data_intro_ex/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._12_spring_data_intro_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
