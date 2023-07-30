9a36725e16e5ca70750995f8be08c6c58ec26bd5
danovska01 <kristina.danovska@gmail.com>
2023-07-30 10:37:43 +0300

    unversioned files added

---
+++ _15_Spring_Data_Auto_Mapping_Objects_Lab/src/test/java/com/example/_15_spring_data_auto_mapping_objects_lab/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._15_spring_data_auto_mapping_objects_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ _16_Spring_Game_Store/src/test/java/com/example/_16_spring_game_store/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._16_spring_game_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
