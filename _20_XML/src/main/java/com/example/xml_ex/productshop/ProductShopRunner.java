fef583bfa688046760ab00e4ab39d3a63f3638ca
danovska01 <kristina.danovska@gmail.com>
2023-08-02 19:13:51 +0300

    adding task 20

--- _20_XML/src/main/java/com/example/xml_ex/productshop/ProductShopRunner.java
+++ _20_XML/src/main/java/com/example/xml_ex/productshop/ProductShopRunner.java
@@ -32,11 +32,18 @@ public class ProductShopRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//        this.seedService.seedProducts();
+//        this.seedService.seedUsers();
+//        this.seedService.seedCategories();
+        this.seedService.seedProducts();
+
+//        this.seedService.seedAll();
+
 
 //        this.productsInRange();
 
-        this.findUserWithSoldProductsAndCounts();
+//        this.findUserWithSoldProductsAndCounts();
+
+
 
     }
 
---
+++ _20_XML/src/test/java/com/danovska01/_20_xml/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danovska01._20_xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
