f8083429c858133c682e34db8f6826f5fdca40b7
danovska01 <kristina.danovska@gmail.com>
2023-10-28 12:19:03 +0300

    fix names

---
+++ E__X__A__M__S/_01_Java_DB_Advanced_Exam_20_Dec_2018_Hiberspring/src/test/java/com/danovska01/exam01/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danovska01.exam01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
